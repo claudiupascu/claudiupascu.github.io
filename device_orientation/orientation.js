2c250dbc4a13650a6a58935f88da0f628c3750b0
claudiupascu <43954709+claudiupascu@users.noreply.github.com>
2018-11-05 13:50:38 +0200

    ver6
    update

--- device_orientation/orientation.js
+++ device_orientation/orientation.js
@@ -1,4 +1,4 @@
-document.getElementById("id_logic_version").innerHTML = "Logic version: 2018.11.05.5";
+document.getElementById("id_logic_version").innerHTML = "Logic version: 2018.11.05.6";
 
 window.addEventListener("deviceorientation", on_device_orientation);
 
@@ -65,10 +65,12 @@ function on_device_motion(e)
 	document.getElementById("id_acc_x").innerHTML = Math.round(acc_x * 100) / 100;
 
 	document.getElementById("id_acc_y").innerHTML = Math.round(acc_y * 100) / 100;
-	
-    document.getElementById("id_rot_x").innerHTML = Math.round(Math.atan(acc_x / acc_z) * 100) / 100;
 
-	document.getElementById("id_rot_y").innerHTML = Math.round(Math.atan(acc_y / acc_z) * 100) / 100;
+	
+
+	document.getElementById("id_rot_x").innerHTML = Math.round(Math.atan(acc_x / acc_z)* 180 / Math.PI * 100) / 100;
+
+	document.getElementById("id_rot_y").innerHTML = Math.round(Math.atan(acc_y / acc_z)* 180 / Math.PI * 100) / 100;
 
 }
 
