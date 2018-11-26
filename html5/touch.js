476f84cce843fad8e24a58abed357d5f9a3d6c34
claudiupascu <43954709+claudiupascu@users.noreply.github.com>
2018-11-26 11:25:56 +0200

    Update html5/touch.js
    display corections

--- html5/touch.js
+++ html5/touch.js
@@ -1,13 +1,14 @@
- document.getElementById("id_bussiness_version").innerHTML = "Bussiness version: 2018.10.22.0";
+ document.getElementById("id_bussiness_version").innerHTML = "Bussiness version: 2018.10.22.2";
  var canvas = document.getElementById('id_canvas');
  canvas.addEventListener("touchstart",on_touch_start);
+ var canvas_bounding_rect = canvas.getBoundingClientRect();
  function on_touch_start(e)
 {
 	for (var i = 0; i < e.changedTouches.length; i++){
 		var context = canvas.getContext("2d");
 		context.beginPath();
-		context.arc(e.changedTouches[i].pageX, 
-					e.changedTouches[i].pageY,
+		context.arc(e.changedTouches[i].pageX- canvas_bounding_rect.top, 
+					e.changedTouches[i].pageY- canvas_bounding_rect.top,
 					10,
 					0,
 					2 * Math.PI);
