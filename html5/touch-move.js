374873491e7c4ac3d8da761f31562b79d38cb2e4
claudiupascu <43954709+claudiupascu@users.noreply.github.com>
2018-11-26 11:39:12 +0200

    Update html5/touch-move.js
    corection

--- html5/touch-move.js
+++ html5/touch-move.js
@@ -2,27 +2,28 @@
  var canvas = document.getElementById('id_canvas');
  canvas.addEventListener("touchstart",on_touch_start);
  var canvas_bounding_rect = canvas.getBoundingClientRect();
- function on_touch_start(e)
+//-------------------------------------------
+function on_touch_start(e)
 {
 	for (var i = 0; i < e.changedTouches.length; i++){
 		var context = canvas.getContext("2d");
 		context.beginPath();
-		context.arc(e.changedTouches[i].pageX- canvas_bounding_rect.top, 
-					e.changedTouches[i].pageY- canvas_bounding_rect.top,
+		context.arc(e.changedTouches[i].pageX - canvas_bounding_rect.left, 
+					e.changedTouches[i].pageY - canvas_bounding_rect.top,
 					10,
 					0,
 					2 * Math.PI);
 		context.stroke();
 	}
 }
+//-------------------------------------------
 function on_touch_move(e)
 {
-	e.preventDefault();
 	for (var i = 0; i < e.changedTouches.length; i++){
 		var context = canvas.getContext("2d");
 		context.beginPath();
-		context.arc(e.changedTouches[i].pageX- canvas_bounding_rect.top, 
-					e.changedTouches[i].pageY- canvas_bounding_rect.top,
+		context.arc(e.changedTouches[i].pageX - canvas_bounding_rect.left, 
+					e.changedTouches[i].pageY - canvas_bounding_rect.top,
 					10,
 					0,
 					2 * Math.PI);
