3a2b5e71ae6d9e5b66bba3c2848486f45b59a701
claudiupascu <43954709+claudiupascu@users.noreply.github.com>
2018-12-03 12:44:23 +0200

    correction
    correction

--- html5/svg.js
+++ html5/svg.js
@@ -9,7 +9,9 @@ function on_device_orientation(e)
 	var svg = document.getElementById("id_svg");
 	var circle = document.getElementById("id_circle");
 	var R = 20;
-    circle.setAttribute("cx",svg.width / 2 + e.gama / 90 * (svg.width /2- R));
-	circle.setAttribute("cx",svg.height / 2 + e.gama / 90 * (svg.height /2- R));
+	var svg.width = svg.getAttribute("width");
+	var svg.height = svg.getAttribute("height");
+    circle.setAttribute("cx",svg.width / 2 + e.gama / 90 * (svg.width /2- R + "px"));
+	circle.setAttribute("cx",svg.height / 2 + e.gama / 90 * (svg.height /2- R + "px"));
 
 }
