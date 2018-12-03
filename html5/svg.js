d0bd93668a9ee42d12972c2abdf9bf01cf41b603
claudiupascu <43954709+claudiupascu@users.noreply.github.com>
2018-12-03 13:06:09 +0200

    debuging script
    debuging

--- html5/svg.js
+++ html5/svg.js
@@ -6,12 +6,13 @@ window.addEventListener("deviceorientation", on_device_orientation);
 function on_device_orientation(e)
 {
 
+
 	var svg = document.getElementById("id_svg");
 	var circle = document.getElementById("id_circle");
 	var R = 20;
 	var svg.width = svg.getAttribute("width");
 	var svg.height = svg.getAttribute("height");
     circle.setAttribute("cx",svg.width / 2 + e.gama / 90 * (svg.width /2- R + "px"));
-	circle.setAttribute("cx",svg.height / 2 + e.gama / 90 * (svg.height /2- R + "px"));
+	circle.setAttribute("cy",svg.height / 2 + e.gama / 90 * (svg.height /2- R + "px"));
 
 }
