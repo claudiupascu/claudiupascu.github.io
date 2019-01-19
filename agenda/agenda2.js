50e743ca9d88a9d04062e3a982b29778b641ea9d
claudiupascu <43954709+claudiupascu@users.noreply.github.com>
2019-01-19 18:49:11 +0200

    setat conditii alarma si introducere formular
    Setat domenii timp ramas pt. alarma si alerta
    Setat conditii introducere formular (nu accepta null)

--- agenda/agenda2.js
+++ agenda/agenda2.js
@@ -1,4 +1,4 @@
-            //functia care defineste continutul initial si campurile de completat ale aplicatiei
+//ver2
 			function eveniment()
 			{
 			   this.nume = "";
@@ -32,22 +32,28 @@
 			}
 			    //functia de adaugare eveniment si ascundere a formularului dupa adaugare
 				
-			function afiseazaFormular(){
+			function afiseazaFormular(){//functia de adaugare eveniment si ascundere a formularului dupa adaugare			
 			   evenim = new eveniment();
 			   evenim.nume = adauga('nume').value;
 			   evenim.email = adauga('email').value;
 			   evenim.telefon = adauga('telefon').value;
 			   evenim.data = adauga('data').value;
 			   evenim.timp = adauga('timp').value;
-			   evenim.evenimentul = adauga('evenimentul').value;
-			   evenimente[evenimente.length] = evenim;
-			   adaugaFormular();
-			   addEvenimente();
+			   evenim.evenimentul = adauga('evenimentul').value;			   
+			   if(evenim.nume != '' && evenim.email != '' && evenim.telefon != '' && evenim.data != '' && evenim.evenimentul != ''){
+				   evenimente[evenimente.length] = evenim;
+			       adaugaFormular();
+			       addEvenimente();
+				  }
+				else
+				{
+					alert("Toate campurile sunt obligatorii");
+				}
 			}
 			    // functia de stergere eveniment
 			function sterge(id_eveniment){
 			   nouEvenim = new Array();
-			   if(confirm(Stergi evenimentul?)){
+			   //if(confirm(Modifici sau stergi evenimentul?)){
 			   for(i=0; i<evenimente.length; i++){
 				   if(i != id_eveniment){
 				   newEvenim[nouEvenim.length] = evenimente[i];
@@ -55,34 +61,33 @@
 			   }
 			   evenimente = nouEvenim;
 			   addEvenimente();
-			   }
+			   //}
 			}
 			
 			   //functia de editare eveniment
-			function edit(id_eveniment){				
+			function edit(id_eveniment){
 				evenim = evenimente[id_eveniment];
 				sterge(id_eveniment);
 				afFormular();
-              if(confirm(Modifici evenimentul?)){			
 				adauga('nume').value = evenim.nume;
 				adauga('email').value = evenim.email;
 				adauga('telefon').value = evenim.telefon;
 				adauga('data').value = evenim.data;
 				adauga('timp').value = evenim.timp;
 				adauga('evenimentul').value = evenim.evenimentul;
-			  }
 			}
 			//navigator.wakeLock.request("system").then(
 			function alarma(){
-				    //preluare data curenta si comparare
-                    // Timp setat
+					 //preluare data curenta si comparare
+                     // Timp setat
                 var timp_setat = new Date(evenim.data+" "+evenim.timp).getTime();          
                     // Actualizare la fiecare secunda
                 var x = setInterval(function() {
                     // Preia data si ora curenta
                 var ora_curenta = new Date().getTime();				
                    // Calculeaza timpul ramas
-                var diferenta = timp_setat - ora_curenta;               			
+                var diferenta = timp_setat - ora_curenta; 
+              			
                    // Calculul timpului ramas in zile , ore , minute si secunde
                 var zile = Math.floor(diferenta / (1000 * 60 * 60 * 24));
                 var ore = Math.floor((diferenta % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
@@ -91,12 +96,12 @@
                    // Amplasamentul afisarii
                  document.getElementById("timp_ramas_eveniment").innerHTML = zile + "zile " + ore + "ore "
                   + minute + "min. " + secunde + "sec. ";
-                   if (diferenta < 60000 && diferenta > 1000 ) {                       
+                   if (diferenta < 60000 && diferenta > 1000) {                       
 			           window.navigator.vibrate([100,30,100,30,100,30,200,30,200,30,200,30,100,30,100,30,100,1000,100,30,100,30,100,30,200,30,200,30,200,30,100,30,100,30,100,1000]);
 				   }
-	               if(diferenta<1000 && diferenta >= 0) {
+	               if(diferenta < 1000 && diferenta >0) {
                        clearInterval(x);
-					    alert("Ai un eveniment in ateptare");
+					   alert("Ai un eveniment in ateptare");
 		           // window.navigator.vibrate([100,30,100,30,100,30,200,30,200,30,200,30,100,30,100,30,100]);
 		          // document.getElementById("timp_ramas_eveniment").innerHTML = "EXPIRAT"; 		   		   
 	              // }
@@ -106,18 +111,21 @@
 				 	//)						
 			   // functia de adaugare eveniment in interiorul html
 			function addEvenimente(){
+			       //alert(evenimente.length);
 			       adauga('toateEvenimentele').innerHTML = '';
 			   for(i=0; i<evenimente.length; i++){
-			           evenim = evenimente[i];				   
-			           div = document.createElement('div');
-				       div.setAttribute('class','eveniment');
-				       div.innerHTML = "<div class='w3-container'>" + "<i>Nume: </i>"+evenim.nume + "<br/>" + "<i>E-mail: </i>" +
-				       evenim.email + "<br/>" + "<i>Telefon: </i>"+ evenim.telefon + "<br/>"+"<i>Data: </i>"+
-				       evenim.data + "<br/>" + "Ora :"+evenim.timp+"<br/>"+"<i>Eveniment: </i>"+ evenim.evenimentul+"<br/>"+
-				       "<button class='w3-button w3-orange' onclick='edit("+i+")'>Edit</button>" + " "+
-				       "<button  class='w3-button w3-red' onclick='sterge("+i+")'>Sterge</button>" + "   "+
-				       "<button  class='w3-button w3-indigo' onclick='alarma()'>Set alarma</button>" + "<br/>" +
-				       "id='timp_ramas_eveniment'" + "<div>";
-				       adauga('toateEvenimentele').appendChild(div);                				   
+			       evenim = evenimente[i];				   
+			       div = document.createElement('div');
+				   div.setAttribute('class','eveniment');
+				   div.innerHTML = "<div class='w3-container'>" + "<i>Nume: </i>"+evenim.nume +"<br/>"+"<i>E-mail: </i>"+
+				   evenim.email +"<br/>"+"<i>Telefon: </i>"+ evenim.telefon +"<br/>"+"<i>Data: </i>"+
+				   evenim.data +"<br/>"+"Ora :"+evenim.timp+"<br/>"+"<i>Eveniment: </i>"+ evenim.evenimentul+"<br/>"+
+				   "<button class='w3-button w3-orange' onclick='edit("+i+")'>Edit</button>" + " "+
+				   "<button  class='w3-button w3-red' onclick='sterge("+i+")'>Sterge</button>"+"   "+
+				   "<button  class='w3-button w3-indigo' onclick='alarma()'>Set alarma</button>"+"<br/>"+
+				   "<p id='timp_ramas_eveniment'></p>" + "<div>";
+				   adauga('toateEvenimentele').appendChild(div);
+			  	   //alert(new Date(evenim.data+" "+evenim.timp));                 				   
+
 			   }
 			}
